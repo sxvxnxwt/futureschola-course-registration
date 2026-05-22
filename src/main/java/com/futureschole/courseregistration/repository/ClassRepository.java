38b75329ac17bb32da8fac6e53887124d1770009
kim minji <gimminji1365@naver.com>
2026-05-22 16:35:19 +0900

    feat: 강의 목록 조회 repository 메서드 추가

--- src/main/java/com/futureschole/courseregistration/repository/ClassRepository.java
+++ src/main/java/com/futureschole/courseregistration/repository/ClassRepository.java
@@ -1,7 +1,13 @@
 package com.futureschole.courseregistration.repository;
 
 import com.futureschole.courseregistration.domain.entity.Class;
+import com.futureschole.courseregistration.domain.enums.ClassStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
+import java.util.List;
+
 public interface ClassRepository extends JpaRepository<Class, Long> {
+
+    List<Class> findAllByStatusInOrderByIdDesc(Collection<ClassStatus> statuses);
 }
